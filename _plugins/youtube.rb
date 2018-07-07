87b2824e9a28c607018a3d09d1737ddd4d3cf59a
Mohit Pokharna <mohitpokharna08@gmail.com>
2018-07-07 19:14:14 +0300

    check

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,39 +1,31 @@
-# Jekyll - Easy Youtube Embed
-#
-# Katie Harron - https://github.com/pibby
-#
-#   Input:
-#     {% youtube Al9FOtZcadQ %}
-#   Output:
-#   <div class="video">
-#     <figure>
-#       <iframe width="640" height="480" src="//www.youtube.com/embed/Al9FOtZcadQ" allowfullscreen></iframe>
-#     </figure>
-#   </div>
+require "jekyll"
+require "jekyll-youtube/version"
+class YouTubeEmbed < Liquid::Tag
 
-module Jekyll
-  class Youtube < Liquid::Tag
-    @url = nil
+  def initialize(tagName, content, tokens)
+    super
+    @content = content
+  end
 
-    VIDEO_URL = /(\S+)/i
-
-    def initialize(tag_name, markup, tokens)
-      super
-
-      if markup =~ VIDEO_URL
-        @url = $1
-      end
+  def render(context)
+    youtube_url = "#{context[@content.strip]}"
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      @youtube_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      @youtube_id = $5
     end
-
-    def render(context)
-      source = "<div class=\"video\">"
-      source += "<figure>"
-      source += "<iframe width=\"640\" height=\"480\" src=\"//www.youtube.com/embed/#{@url}\" frameborder=\"0\" allowfullscreen></iframe>"
-      source += "</figure>"
-      source += "</div>"
-      source
+    
+    tmpl_path = File.join Dir.pwd, "_includes", "youtube.html"
+    if File.exist?(tmpl_path)
+      tmpl = File.read tmpl_path
+      site = context.registers[:site]
+      tmpl = (Liquid::Template.parse tmpl).render site.site_payload.merge!({"youtube_id" => @youtube_id})
+    else
+      %Q{<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'>    <iframe title="YouTube video player" width="640" height="390" src="//www.youtube.com/embed/#{ @youtube_id }" frameborder="0" allowfullscreen></iframe></div>}
     end
   end
-end
 
-Liquid::Template.register_tag('youtube', Jekyll::Youtube)
+  Liquid::Template.register_tag "youtube", self
+end
